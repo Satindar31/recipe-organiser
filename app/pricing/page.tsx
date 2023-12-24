620c2ed475491d88dc9795e793a491072ef6588a
Satindar <ibrahimasif711@outlook.com>
2023-12-24 13:31:01 +0530

    untested

--- app/pricing/page.tsx
+++ app/pricing/page.tsx
@@ -5,6 +5,10 @@ export default function Pricing() {
   return (
     <div>
       <NavbarComponent />
+      <h1 className="text-4xl font-bold text-center mt-10">Pricing</h1>
+      <h2 className="text-2xl font-bold text-center mt-5">
+        Choose the best plan for you
+      </h2>
       <PricingTableComponent />
     </div>
   );
