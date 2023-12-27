8dbf5d38323ae8920ec02dc2625e0dd4eb8c759e
Satindar <ibrahimasif711@outlook.com>
2023-12-27 22:26:42 +0530

    untested again

--- app/recipies/page.tsx
+++ app/recipies/page.tsx
@@ -1,31 +1,11 @@
+import { FeaturedRecipiesPage } from "@/components/component/featured-recipies-page";
 import NavComponent from "@/components/misc/navbar";
-import { IRecipe } from "@/interfaces/recipe";
 
 export default async function RecipiesPage() {
-  const res = await fetch(process.env.URL + "/api/featured/recipies", {
-    method: "POST",
-    body: JSON.stringify({
-      order: "newest",
-    }),
-    cache: "force-cache",
-    next: {
-      revalidate: 43200,
-    },
-  });
-  const data: IRecipe[] = await res.json();
   return (
-    <div>
-      <NavComponent />
-      <h1 className="text-5xl">Recipies</h1>
-      <h2>Newest recipies</h2>
-      <div className="grid grid-cols-2 grid-rows-2">
-        {data.map((recipe) => (
-          <div key={recipe.id.toString()}>
-            <p className="text-black">{recipe.name}</p>
-            <p className="text-gray-800">{recipe.description}</p>
-          </div>
-        ))}
-      </div>
-    </div>
+    <>
+    <NavComponent />
+    <FeaturedRecipiesPage />
+    </>
   );
 }
